428211aba425a8248dc7463662e86a9a20221477
debms <deboramedina200493@gmail.com>
2022-01-25 22:37:16 -0300

    correccion complemento 2

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const ItemCount = (props) => {
-  const { stock, initial, onAdd } = props;
+  const { stock, initial, setContador } = props;
+  const onAdd = (e, count, stock) => {
+    if (e.target.matches(".aumentar")) {
+      if (count <= stock - 1) {
+        setContador(initial + 1);
+      }
+    }
+    if (e.target.matches(".decrementar")) {
+      if (count >= 2) {
+        setContador(initial - 1);
+      }
+    }
+  };
   return (
     <div className="contadorProductos">
       <button className="decrementar" onClick={(e) => onAdd(e, initial, stock)}>
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -3,18 +3,7 @@ import ItemCount from "./ItemCount";
 import "./components.css";
 const ItemListContainer = (props) => {
   const [contador, setContador] = useState(props.initial);
-  const onAdd = (e, count, stock) => {
-    if (e.target.matches(".aumentar")) {
-      if (count <= stock - 1) {
-        setContador(contador + 1);
-      }
-    }
-    if (e.target.matches(".decrementar")) {
-      if (count >= 2) {
-        setContador(contador - 1);
-      }
-    }
-  };
+
   return (
     <div className="oferta__contenedor">
       <p className="oferta">{props.greeting}</p>
@@ -23,7 +12,7 @@ const ItemListContainer = (props) => {
       <ItemCount
         initial={contador}
         stock={props.stock}
-        onAdd={onAdd}
+        setContador={setContador}
       ></ItemCount>
     </div>
   );
